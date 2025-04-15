7a873828cea251c10ee546d3ccb53f536bd2e5da
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-15 21:42:12 +0200

    save

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const ws = new WebSocket('wss://pesentiws-43f6274c0f11.herokuapp.com/');
 
-const PREFIX = "TELECOMANDO:";
+let config = {
+  uscita: {
+    isOpen: false,
+    button: document.getElementById("uscitaButton")
+  },
+  ingresso: {
+    isOpen: false,
+    button: document.getElementById("ingressoButton")
+  }
+};
+
+Object.keys(config).forEach(key => {
+  const thisButton = config[key].button;
+  thisButton.textContent = config[key].isOpen ? "APERTO" : "CHIUSO";
+  thisButton.style.backgroundColor = config[key].isOpen ? "green" : "red";
+  thisButton.addEventListener("click", () => {
+    config[key].isOpen = !config[key].isOpen;
+    thisButton.textContent = config[key].isOpen ? "APERTO" : "CHIUSO";
+    thisButton.style.backgroundColor = config[key].isOpen ? "green" : "red";
+    ws.send(key + ":" + Number(config[key].isOpen));
+  });
+});
+
+
+
+
+
 
 
 
