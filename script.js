c00dc1193a7fbc60ac1686d91958b6f22b94ebd7
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-05-28 17:41:32 +0200

    save

--- script.js
+++ script.js
@@ -1,47 +1,45 @@
-const ws = new WebSocket('wss://pesentiws-43f6274c0f11.herokuapp.com/');
+const ws = new WebSocket("wss://pesentiws-43f6274c0f11.herokuapp.com/");
 
 let config = {
   uscita: {
     isOpen: false,
-    button: document.getElementById("uscitaButton")
+    button: document.getElementById("uscitaButton"),
   },
   ingresso: {
     isOpen: false,
-    button: document.getElementById("ingressoButton")
-  }
+    button: document.getElementById("ingressoButton"),
+  },
 };
 
 ws.addEventListener("open", () => {
-  document.getElementById("wsClose").hidden = true;
-  document.getElementById("wsOpen").hidden = false;
-  
-  Object.keys(config).forEach(key => {
+  Object.keys(config).forEach((key) => {
     ws.send(key + ":" + Number(config[key].isOpen));
   });
 });
 
 ws.addEventListener("close", () => {
-  document.getElementById("wsClose").hidden = false;
-  document.getElementById("wsOpen").hidden = true;
+  window.location.reload();
 });
 
+ws.addEventListener("message", (e) => {
+  const [key, value] = e.data.split(":");
+  if (key == "cancelloCameraInput") {
+    console.log({value});
+  }
+})
 
-Object.keys(config).forEach(key => {
+Object.keys(config).forEach((key) => {
   const thisButton = config[key].button;
   thisButton.textContent = config[key].isOpen ? "APERTO" : "CHIUSO";
-  thisButton.style.backgroundColor = config[key].isOpen ? "rgb(0, 255, 123)" : "rgb(255, 0, 8)";
+  thisButton.style.backgroundColor = config[key].isOpen
+    ? "rgb(0, 255, 123)"
+    : "rgb(255, 0, 8)";
   thisButton.addEventListener("click", () => {
     config[key].isOpen = !config[key].isOpen;
     thisButton.textContent = config[key].isOpen ? "APERTO" : "CHIUSO";
-    thisButton.style.backgroundColor = config[key].isOpen ? "rgb(0, 255, 123)" : "rgb(255, 0, 8)";
+    thisButton.style.backgroundColor = config[key].isOpen
+      ? "rgb(0, 255, 123)"
+      : "rgb(255, 0, 8)";
     ws.send(key + ":" + Number(config[key].isOpen));
   });
 });
-
-
-
-
-
-
-
-
